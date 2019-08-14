804021e22640758be366bf487ecf8a1295f06f41
Alexandru1Lefter <lefter.alexandru.i@gmail.com>
2019-08-14 21:20:47 +0300

    Random numere cu 2 clase

---
+++ Number1.java
@@ -0,0 +1,16 @@
+package com.teste.negru;
+
+public class Number1 {
+
+    public static int primulNumar (){
+        int a = (int) (Math.random()*11);
+        return a;
+    }
+
+    public static int alDoileaNumar(){
+        int b = (int) (Math.random()*11);
+        return b;
+
+    }
+}
+
