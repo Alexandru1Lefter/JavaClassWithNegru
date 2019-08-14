f55991bd817e6187f94f0c4d938dafeb7a7c7816
Alexandru1Lefter <lefter.alexandru.i@gmail.com>
2019-08-14 21:11:04 +0300

    Random numere cu 2 clase

---
+++ Main.java
@@ -0,0 +1,28 @@
+package com.teste.negru;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Number1 x = new Number1();
+        Number1 y = new Number1();
+
+        x.primulNumar();
+        y.alDoileaNumar();
+
+        int nrX = x.primulNumar();
+        int nrY = y.alDoileaNumar();
+
+        System.out.println(nrX);
+        System.out.println(nrY);
+
+
+        while (nrX != nrY) {
+            System.out.println("Mai generam o data numere");
+
+        }
+        System.out.println(nrX + " este egal cu " + nrY);
+
+    }
+}
+
