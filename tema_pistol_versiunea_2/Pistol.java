b308904c710dbd18491b768a27cfdb8a60c4cc8a
Alexandru1Lefter <53571607+Alexandru1Lefter@users.noreply.github.com>
2019-08-25 13:44:09 +0300

    deleted some unnecessary classes

---
+++ tema_pistol_versiunea_2/Pistol.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema_pistol_versiunea_2;
+
+/**
+ *
+ * @author Lefter
+ */
+public class Pistol {
+
+    //final int CAPACITATE_INCARCATOR;
+    
+    boolean piedica;
+    Incarcator incarcatorPistol;
+
+   // public Pistol(int CAPACITATE_INCARCATOR) {
+   //     this.CAPACITATE_INCARCATOR = CAPACITATE_INCARCATOR;
+   // }
+
+    public void schimbaIncarcator(Incarcator i) {
+         incarcatorPistol = i;
+        
+    }
+    
+    public void Incarca(Incarcator i){
+        
+    }
+
+    public void trage() {
+        if (piedica) {
+            System.out.println("Atentie! Piedica este pusa!");
+        } else if ( incarcatorPistol.nrGloante == 0) {
+            System.out.println("Nu mai ai gloante! Incarca!");
+
+        } else {
+            incarcatorPistol.nrGloante--;
+            int sansa = (int) (Math.random() * 2);
+            System.out.println("Pew pew! Pew pew!");
+            if (sansa == 0) {
+                System.out.println("Ai ratat...");
+                System.out.println("Mai ai " + incarcatorPistol.nrGloante + " gloante");
+            } else {
+                System.out.println("Ai nimerit!");
+                System.out.println("Mai ai " + incarcatorPistol.nrGloante + " gloante");
+            }
+        }
+    }
+
+    public void punePiedica() {
+        if (piedica) {
+            System.out.println("Piedica deja pusa");
+        } else {
+            System.out.println("Piedica pusa!");
+            this.piedica = true;
+        }
+    }
+
+    public void scoatePiedica() {
+        if (!piedica) {
+            System.out.println("Piedica deja scoasa");
+        } else {
+            System.out.println("Piedica scoasa!");
+            this.piedica = false;
+        }
+    }
+}
---
+++ tema_pistol_versiunea_2/TestPistol.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema_pistol_versiunea_2;
+
+/**
+ *
+ * @author Lefter
+ */
+public class TestPistol {
+    
+    public static void main(String[] args) {
+        
+        Pistol p1 = new Pistol();
+        Incarcator i1 = new Incarcator(20,2);
+        p1.schimbaIncarcator(i1);
+        System.out.println("Incarcator i gloante : "+i1.nrGloante+" / Capacitate incarcator i: "+i1.capacitateIncarcator);
+       p1.trage();
+       p1.trage();
+       p1.trage();
+       i1.Incarca(20);
+       Incarcator i2 = new Incarcator (5,5);
+       p1.schimbaIncarcator(i2);
+       p1.trage();
+       p1.trage();
+       p1.schimbaIncarcator(i1);
+       p1.trage();
+    }
+    
+}
